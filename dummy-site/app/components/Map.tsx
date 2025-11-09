bc6d4e9052b381fcb69387d92c202b5c146fc748
Logan Young <younglogan09@gmail.com>
2025-11-09 14:37:33 +1200

    fixed map

--- dummy-site/app/components/Map.tsx
+++ dummy-site/app/components/Map.tsx
@@ -2,18 +2,10 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
 
-// Fix for default markers in react-leaflet
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-});
-
+// Simple solution - let react-leaflet handle the icon
 export default function Map() {
-  const position = [-40.352306, 175.590396]; // Coordinates for 28 Shamrock Street
+  const position: [number, number] = [-40.352306, 175.590396];
 
   return (
     <MapContainer
