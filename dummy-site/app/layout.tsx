c94a04981039e8daeb103119cd866a20f6d962ea
Logan Young <younglogan09@gmail.com>
2025-12-09 22:02:59 +1300

    updated

--- dummy-site/app/layout.tsx
+++ dummy-site/app/layout.tsx
@@ -1,34 +1,36 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-import ConvexClientProvider from '@/components/ConvexClientProvider'
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvider from "@/components/ConvexClientProvider";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Happy Horizons',
-  description: 'Built by Logan Young (Start)16/10/2025',
-}
+  title: "Happy Horizons",
+  description: "Built by Logan Young (Start)16/10/2025",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <Navbar />
         <ClerkProvider>
           <ConvexClientProvider>{children}</ConvexClientProvider>
@@ -36,5 +38,5 @@ export default function RootLayout({
         <Footer />
       </body>
     </html>
-  )
+  );
 }
